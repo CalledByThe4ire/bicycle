22f398d579eeb55e1ce059e9ab6fccc2656b1b72
Andrey Karamnoff <karamnoff@gmail.com>
2023-03-05 16:10:33 +0300

    Add icon, typography

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { GlobalResets } from './tokens/resets';
 import { GlobalFonts } from './tokens/fonts';
-import { GlobalColors } from './tokens/colors';
+import { GlobalColors, Color } from './tokens/colors';
 import { GlobalShadows } from './tokens/shadows';
 import { GlobalRadii } from './tokens/radii';
-import { SpriteWithIcons } from './tokens/icons';
+import { IconName, SpriteWithIcons } from './tokens/icons';
+
+import { TypographyVariant, Typography } from './elements/typography';
+
+import { Icon } from './elements/icon';
 
 function App() {
   return (
@@ -14,6 +18,13 @@ function App() {
       <GlobalShadows />
       <GlobalRadii />
       <SpriteWithIcons />
+
+      <Icon name={IconName.BIKE_ADULT} />
+      <Icon name={IconName.BIKE_CHILD} color={Color.GREEN_30} />
+
+      <Typography as='h1' variant={TypographyVariant.TITLE_1} color={Color.ORANGE_30}>
+        Магазин велосипедов «Велик»
+      </Typography>
     </>
   );
 }
